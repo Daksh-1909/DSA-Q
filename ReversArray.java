a372ba230c83a3dd835ef889158c1fcac6f5e8fa
Daksh-1909 <141130860+Daksh-1909@users.noreply.github.com>
2026-07-13 10:29:27 +0530

    new code

---
+++ ReversArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ReversArray {
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter 5 elements of array:");
+
+        for (int i=0; i<5;i++) {
+            arr[i]=s.nextInt();
+        }
+        for (int i=0; i<arr.length/2;i++) {
+            int temp=arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+        System.out.println("Reversed array:");
+        for (int i=0; i<arr.length;i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
