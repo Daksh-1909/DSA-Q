e7f46805687a754e0c1260525d0e2e22beec48f8
Daksh-1909 <141130860+Daksh-1909@users.noreply.github.com>
2026-07-13 11:41:32 +0530

    -

--- TwoSum.java
+++ TwoSum.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int n = 6;
+        int n=6;
         int[] arr=new int[n];
         Scanner s=new Scanner(System.in);
-        System.out.println("Enter " + n + " elements of array:");
+        System.out.println("Enter "+n+" elements of array:");
         for (int i=0;i<n;i++) {
             arr[i]=s.nextInt();
         }
         System.out.println("Enter the target sum:");
         int target=s.nextInt();
 
-        Map<Integer, Integer> numMap = new HashMap<>();
-        boolean pairFound = false;
-        for (int num : arr) {
-            int temp = target - num;
+        Map<Integer, Integer> numMap=new HashMap<>();
+        boolean pairFound=false;
+        for (int num:arr) {
+            int temp=target-num;
             if (numMap.containsKey(temp)) {
-                System.out.println("Pair is: " + temp + ", " + num);
-                pairFound = true;
+                System.out.println("Pair is: "+temp+", "+num);
+                pairFound=true;
             }
-            numMap.put(num, 1); 
+            numMap.put(num,1); 
         }
     }
 }
