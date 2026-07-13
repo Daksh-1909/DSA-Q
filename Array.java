c2ca3bf98552791addd945848fabc4da65d63b68
Daksh-1909 <141130860+Daksh-1909@users.noreply.github.com>
2026-07-13 09:45:44 +0530

    new code

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 5 elements of array: ");
+        for (int i=0; i<5; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<5; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
